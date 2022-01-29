5bfacfcaf7d2deab98edc61f97598ca45a2d1b45
Marina <mp@friendwork.ru>
2022-01-29 12:28:05 +0300

    Router

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
 import express from 'express';
+import { userRouter } from './users/users.js';
 
 const port = 8000;
 const app = express();
 
+// app.all('/hello', (req, res, next) => {
+//   console.log('ALL');
+//   next();
+// })
+
+// app.route('/user')
+//   .get('/hello', (req, res) => {
+//     res.send('Привет, GET!')
+//   })
+//   .post('/hello', (req, res) => {
+//     console.log('POST');
+//     res.send('Привет, POST')
+//   })
+
 app.get('/hello', (req, res) => {
-  res.send('Привет!')
+  res.send('Привет от GET')
 })
+
+app.use('/user', userRouter);
 
 app.listen(port, () => {
   console.log(`Сервер запущен на http://localhost:${port}`)
