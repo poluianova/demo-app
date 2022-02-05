b2b2f883e2937813ea35f5bf6ece2279527a31fd
Marina <mp@friendwork.ru>
2022-02-05 11:25:54 +0300

    BaseController

---
+++ src/common/base.controller.ts
@@ -0,0 +1,36 @@
+import { Router, Response } from 'express';
+import { LoggerService } from '../logger/logger.service';
+import { IControllerRoute } from './route.interface';
+
+export abstract class BaseController {
+  private readonly _router: Router;
+
+  constructor(private logger: LoggerService) {
+    this._router = Router();
+  }
+
+  get router() {
+    return this._router;
+  }
+
+  public ok<T>(res: Response, message: T) {
+    this.send<T>(res, 200, message);
+  }
+
+  public send<T>(res: Response, code: number, message: T) {
+    res.type('application/json');
+    return res.status(code).json(message);
+  }
+
+  public created(res: Response) {
+    return res.sendStatus(201);
+  }
+
+  protected bindRouters(routers: IControllerRoute[]) {
+    for (const route of routers) {
+      this.logger.log(`[${route.method}] ${route.path}`);
+      const handler = route.func.bind(this);
+      this.router[route.method](route.path, handler);
+    }
+  }
+}
